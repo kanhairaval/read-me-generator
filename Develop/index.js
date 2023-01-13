d74e73e64f27eb85f3c364b2b030506e956ba710
kanhairaval <kanhairaval@gmail.com>
2023-01-13 03:58:22 -0500

    stuck at creating file, haven't started with license yet

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,78 @@
 // TODO: Include packages needed for this application
-const inquirermod = require("inquirer");
+const inquirerMod = require("inquirer");
+const fileMod = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+  }, 
+  {
+    type: "input",
+    name: "description",
+    message: "Please, provide a brief description of the project?"
+  }, 
+  {
+    type: "checkbox",
+    name: "content",
+    message: "What is the table of content?",
+    choices: ["Description", "Installation", "Usage", "License", "Contrubutors'", "Testing", "GitHub", "Contact"]
+  }, 
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation insctructions?"
+  }, 
+  {
+    type: "input",
+    name: "usage",
+    message: "How do I use the application?"
+  }, 
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license have you used?",
+    choices: ["isc", "gpl", "mit", "ncsa", "No License"]
+  }, 
+  {
+    type: "input",
+    name:"contributors",
+    message: "Who are the contributors?"
+  }, 
+  {
+    type: "input",
+    name: "testing",
+    message: "Any testing required?"
+  }, 
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username link?"
+  }, 
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?"
+  }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirerMod.prompt(questions)
+  .then(function (data) {
+    function writeToFile(fileName, data) {
+      const readMeFile = "LISEZMOI.md";
+    
+      fileMod.writeFile(readMeFile, JSON.stringify(data, null, "\t"), (err) =>
+        err ? console.log(err) : console.log("Success!")
+        );
+    }
+  })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const isc = [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +16,32 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Table of Content
+  - ${data.content}
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installaion}
+
+  ##Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Testing
+  ${data.testing}
+
+  ## Github
+  ${data.username}
+
+  ## Contact Info
+  ${data.email}
 `;
 }
 
